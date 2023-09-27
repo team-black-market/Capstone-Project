221ff8c124b24202e6ffeb1a7d76df2cf8d63d4d
Eric Katz <ericpkatz@gmail.com>
2023-09-27 11:25:28 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const pg = require('pg');
+const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const homePage = path.join(__dirname, 'index.html');
+app.get('/', (req, res)=> res.sendFile(homePage));
+
+const reactApp = path.join(__dirname, 'dist/main.js');
+app.get('/dist/main.js', (req, res)=> res.sendFile(reactApp));
+
+const reactSourceMap = path.join(__dirname, 'dist/main.js.map');
+app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
+
+const styleSheet = path.join(__dirname, 'styles.css');
+app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
+
+const init = async()=> {
+  await client.connect();
+  console.log('connected to database');
+  const SQL = `
+    SQL SETUP AND SEED
+  `;
+  console.log('create your tables and seed data');
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, ()=> {
+    console.log(`listening on port ${port}`);
+  });
+}
+
+init();
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = ()=> {
+  return (
+    <h1>FullStack Template</h1>
+  );
+};
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<App />);
