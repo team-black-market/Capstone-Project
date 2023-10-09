df113c2cad01df5cfbf52c9c59118eb3b3f1c6f9
Kyra Polk <kyraambeau@gmail.com>
2023-10-09 11:07:21 -0500

    seeded data, added price,description, and quantity for products

--- server/db/index.js
+++ server/db/index.js
@@ -39,7 +39,10 @@ const seed = async()=> {
     CREATE TABLE products(
       id UUID PRIMARY KEY,
       created_at TIMESTAMP DEFAULT now(),
-      name VARCHAR(100) UNIQUE NOT NULL
+      name VARCHAR(100) UNIQUE NOT NULL,
+      price INTEGER,
+      description TEXT,
+      quantity INTEGER
     );
 
     CREATE TABLE orders(
@@ -67,10 +70,10 @@ const seed = async()=> {
     createUser({ username: 'ethyl', password: '1234', is_admin: true})
   ]);
   const [foo, bar, bazz] = await Promise.all([
-    createProduct({ name: 'foo' }),
-    createProduct({ name: 'bar' }),
-    createProduct({ name: 'bazz' }),
-    createProduct({ name: 'quq' }),
+    createProduct({ name: 'The Bat Mobile', price:100000000, description:'The infamous mode of transportation for one of the most prestigious heroes in gotham, Batman!', quantity: 1}),
+    createProduct({ name: 'The Lasso of Truth', price:265000, description:'A weapon wielded by none other than Wonder Woman herself!', quantity: 1 }),
+    createProduct({ name: 'The Mark I', price:1998500, description:'The first suit ever designed and created by Tony Stark. One of a kind.', quantity: 1 }),
+    createProduct({ name: 'Baby Picture of Morgan Freeman', price:9004, description:'A rare sight none have seen before. A youthful picture of the age defying legend!', quantity:2 }),
   ]);
   let orders = await fetchOrders(ethyl.id);
   let cart = orders.find(order => order.is_cart);
