9f5359ebc7a4f5d9869a80027a0c2dbaf090f76a
Alonzo <alonzo@Alonzo>
2023-10-09 11:50:33 -0600

    Added single file view, re-ordered file structure, created add product function in index.js

--- src/Authorized/Products.js
+++ src/Authorized/Products.js
@@ -5,13 +5,16 @@ const Products = ({ products, cartItems, createLineItem, updateLineItem, auth})=
   return (
     <div>
       <h2>Products</h2>
+      <Link to='/NewProduct'>Add a Product</Link>
       <ul>
         {
           products.map( product => {
             const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
             return (
               <li key={ product.id }>
-                { product.name }
+                <Link to={`/products/${product.id}`}>
+                  { product.name }
+                </Link>
                 {
                   auth.id ? (
                     cartItem ? <button onClick={ ()=> updateLineItem(cartItem)}>Add Another</button>: <button onClick={ ()=> createLineItem(product)}>Add</button>
