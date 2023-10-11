afb18cfbc260ec8d26255ac4b22b55eb4d5daab8
Alonzo <alonzo@Alonzo>
2023-10-10 22:29:29 -0600

    Reconfigured routes, added single page views, added cart page text for nothing in cart.

--- src/Authorized/AuthHome.js
+++ src/Authorized/AuthHome.js
@@ -7,16 +7,6 @@ import Cart from './Cart';
 const AuthHome = ({products, orders, cartCount, auth, logout, cartItems, createLineItem, updateLineItem, cart, lineItems, updateOrder, removeFromCart, wishlist, setWishlist})=> {
     return (
         <>
-            <nav className='navBar'>
-              <Link to='/products'>Products ({ products.length })</Link>
-              <Link to='/orders'>Orders ({ orders.filter(order => !order.is_cart).length })</Link>
-              <Link to='/cart'>Cart ({ cartCount })</Link>
-              <Link to='/wishlist'>Wishlist ({wishlist.length})</Link>
-              <span>
-                Welcome { auth.username }!
-                <button onClick={ logout }>Logout</button>
-              </span>
-            </nav>
             <main>
               <Products
                 auth = { auth }
@@ -26,18 +16,6 @@ const AuthHome = ({products, orders, cartCount, auth, logout, cartItems, createL
                 updateLineItem = { updateLineItem }
                 wishlist={ wishlist }
                 setWishlist={ setWishlist }
-              />
-              <Cart
-                cart = { cart }
-                lineItems = { lineItems }
-                products = { products }
-                updateOrder = { updateOrder }
-                removeFromCart = { removeFromCart }
-              />
-              <Orders
-                orders = { orders }
-                products = { products }
-                lineItems = { lineItems }
               />
             </main>
         </>
