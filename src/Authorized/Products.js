2d5ad43dd61bc6adf73fcb3ce9497f1efd0dd640
Alonzo <alonzo@Alonzo>
2023-10-13 14:01:31 -0600

    test

--- src/Authorized/Products.js
+++ src/Authorized/Products.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
+import api from '../api';
 
 
-const Products = ({ products, cartItems, createLineItem, updateLineItem, auth})=> {
+const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, wishlist, setWishlist})=> {
   const navigate = useNavigate();
   const { term } = useParams();
   return (
     <div>
       <h2>Products</h2>
-      {auth.is_admin ? ( <Link to='/NewProduct'>Add a Product</Link> ) : null}<br/><br/>
+        <br/><br/>
         <input placeholder='Search our inventory' value={ term || '' } onChange={ev => navigate(ev.target.value ? `/products/search/${ev.target.value}`: '/products')}/><br/>
         <ul id="products">
         {
@@ -30,6 +31,59 @@ const Products = ({ products, cartItems, createLineItem, updateLineItem, auth})=
                   ): null
                 }
               </li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
+
+const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, wishlist, setWishlist})=> {
+  const navigate = useNavigate();
+  const { term } = useParams();
+  return (
+    <div>
+      <input className='searchBar' placeholder='Search our inventory' value={ term || '' } onChange={ev => navigate(ev.target.value ? `/products/search/${ev.target.value}`: '/products')}/>
+      <ul id="products">
+        {
+          products.filter(product => !term || product.name.includes(term)).map( product => {
+            const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
+            const favorite = wishlist.find(wishItem => wishItem.product_id === product.id);
+            return (
+              <div className='productContainer' key={ product.id }>
+                <div id='productHeader'>
+                  <div>
+                    <p>${(product.price).toFixed(2)}</p>
+                    <p>Qty: {product.quantity}</p>
+                  </div>
+                  {
+                    favorite ? 
+                    <img onClick={()=> api.removeFromWishlist({userId: auth.id, wishItem: favorite, setWishlist, wishlist})} key="wishItem.id" src="../assets/img/favorite.svg"/>
+                    : <img onClick={()=> api.addToWishList({userId: auth.id, wishItem: product, setWishlist, wishlist})} key="wishItem.id" src="../assets/img/notFavorite.svg"/>
+                  }
+                </div>
+                <div id='productImage'>
+                  <img src={product.image_url}/>
+                </div>
+                <div id='productFooter'>
+                  <Link to={`/products/${product.id}`}>
+                    { product.name }
+                  </Link>
+                  {
+                    auth.id ? (
+                      cartItem ? <button onClick={ ()=> updateLineItem(cartItem)}>Add Another</button>: <button onClick={ ()=> createLineItem(product)}>Add</button>
+                    ): null 
+                  }
+                  {
+                    auth.is_admin ? (
+                      <Link to={`/products/${product.id}/edit`}>Edit</Link>
+                    ): null
+                  }  
+                </div>
+              </div>
             );
           })
         }
