5b99d9375a32f18a0e5f2b86a69b8a9642c0e80a
Alonzo <alonzo@Alonzo>
2023-10-14 20:42:54 -0600

    Displaying vip only products for a vip.

--- src/Authorized/Products.js
+++ src/Authorized/Products.js
@@ -12,8 +12,53 @@ const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, w
         <input placeholder='Search' value={ term || '' } onChange={ev => navigate(ev.target.value ? `/products/search/${ev.target.value}`: '/products')}/>
       </div>
         <ul id="products">
-        {
-          products.filter(product => !term || product.name.includes(term)).map( product => {
+        { 
+          auth.is_vip ? 
+            products.filter(product => !term || product.name.includes(term)).map( product => {
+              const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
+              const favorite = wishlist.find(wishItem => wishItem.product_id === product.id);
+              return (
+              <div className='productContainer' key={ product.id }>
+                  <div id='productHeader'>
+                    <div>
+                      <p>${(product.price).toFixed(2)}</p>
+                      <p>Qty: {product.quantity}</p>
+                    </div>
+                    <div>
+                      {
+                        product.for_vip ? <img className='icon' src='../assets/img/vipIcon.svg'/> : null
+                      }
+                      &nbsp;&nbsp;
+                      {
+                        favorite ? 
+                        <img onClick={()=> api.removeFromWishlist({userId: auth.id, wishItem: favorite, setWishlist, wishlist})} key="wishItem.id" src="../assets/img/favorite.svg"/>
+                        : <img onClick={()=> api.addToWishList({userId: auth.id, wishItem: product, setWishlist, wishlist})} key="wishItem.id" src="../assets/img/notFavorite.svg"/>
+                      }
+                    </div>
+                  </div>
+                  <div id='productImage'>
+                    <img src={product.image_url}/>
+                  </div>
+                  <div id='productFooter'>
+                    <Link to={`/products/${product.id}`}>
+                      { product.name }
+                    </Link>
+                    {
+                      auth.id ? (
+                        cartItem ? <button className='buttonStyle'onClick={ ()=> updateLineItem(cartItem)}>Add Another</button>: <button onClick={ ()=> createLineItem(product)}>Add</button>
+                      ): null 
+                    }
+                    {
+                      auth.is_admin ? (
+                        <Link to={`/products/${product.id}/edit`}>Edit</Link>
+                      ): null
+                    }  
+                  </div>
+                </div>
+              );
+            })
+          : 
+          products.filter(product => (!term || product.name.includes(term)) && !product.for_vip).map( product => {
             const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
             const favorite = wishlist.find(wishItem => wishItem.product_id === product.id);
             return (
@@ -23,11 +68,17 @@ const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, w
                     <p>${(product.price).toFixed(2)}</p>
                     <p>Qty: {product.quantity}</p>
                   </div>
-                  {
-                    favorite ? 
-                    <img onClick={()=> api.removeFromWishlist({userId: auth.id, wishItem: favorite, setWishlist, wishlist})} key="wishItem.id" src="../assets/img/favorite.svg"/>
-                    : <img onClick={()=> api.addToWishList({userId: auth.id, wishItem: product, setWishlist, wishlist})} key="wishItem.id" src="../assets/img/notFavorite.svg"/>
-                  }
+                  <div>
+                    {
+                      product.for_vip ? <img className='icon' src='../assets/img/vipIcon.svg'/> : null
+                    }
+                    &nbsp;&nbsp;
+                    {
+                      favorite ? 
+                      <img onClick={()=> api.removeFromWishlist({userId: auth.id, wishItem: favorite, setWishlist, wishlist})} key="wishItem.id" src="../assets/img/favorite.svg"/>
+                      : <img onClick={()=> api.addToWishList({userId: auth.id, wishItem: product, setWishlist, wishlist})} key="wishItem.id" src="../assets/img/notFavorite.svg"/>
+                    }
+                  </div>
                 </div>
                 <div id='productImage'>
                   <img src={product.image_url}/>
@@ -57,56 +108,3 @@ const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, w
 };
 
 export default Products;
-
-// const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, wishlist, setWishlist})=> {
-//   const navigate = useNavigate();
-//   const { term } = useParams();
-//   return (
-//     <div>
-//       <input className='searchBar' placeholder='Search our inventory' value={ term || '' } onChange={ev => navigate(ev.target.value ? `/products/search/${ev.target.value}`: '/products')}/>
-//       <ul id="products">
-//         {
-//           products.filter(product => !term || product.name.includes(term)).map( product => {
-//             const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
-//             const favorite = wishlist.find(wishItem => wishItem.product_id === product.id);
-//             return (
-//               <div className='productContainer' key={ product.id }>
-//                 <div id='productHeader'>
-//                   <div>
-//                     <p>${(product.price).toFixed(2)}</p>
-//                     <p>Qty: {product.quantity}</p>
-//                   </div>
-//                   {
-//                     favorite ? 
-//                     <img onClick={()=> api.removeFromWishlist({userId: auth.id, wishItem: favorite, setWishlist, wishlist})} key="wishItem.id" src="../assets/img/favorite.svg"/>
-//                     : <img onClick={()=> api.addToWishList({userId: auth.id, wishItem: product, setWishlist, wishlist})} key="wishItem.id" src="../assets/img/notFavorite.svg"/>
-//                   }
-//                 </div>
-//                 <div id='productImage'>
-//                   <img src={product.image_url}/>
-//                 </div>
-//                 <div id='productFooter'>
-//                   <Link to={`/products/${product.id}`}>
-//                     { product.name }
-//                   </Link>
-//                   {
-//                     auth.id ? (
-//                       cartItem ? <button onClick={ ()=> updateLineItem(cartItem)}>Add Another</button>: <button onClick={ ()=> createLineItem(product)}>Add</button>
-//                     ): null 
-//                   }
-//                   {
-//                     auth.is_admin ? (
-//                       <Link to={`/products/${product.id}/edit`}>Edit</Link>
-//                     ): null
-//                   }  
-//                 </div>
-//               </div>
-//             );
-//           })
-//         }
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Products;
