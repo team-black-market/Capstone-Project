aad7418752ca0f06c38019267b98ad706bd87136
Alonzo <alonzo@Alonzo>
2023-10-17 21:21:31 -0600

    Fixed bug, added button styles

--- src/Authorized/Product.js
+++ src/Authorized/Product.js
@@ -14,12 +14,12 @@ const Product = ({products, newReview, reviews, setReviews, productTags})=> {
             <h3>Posted on {product.created_at.slice(0, 10)}</h3>
             <p>{product.description}</p>
             <img className='imageSize' src={product.image_url}></img><br/>
-            {p_tags ? (p_tags.is_accessory ? <button>#Accessory</button> : null) : null}
-            {p_tags ? (p_tags.is_substance ? <button>#Substance</button> : null) : null}
-            {p_tags ? (p_tags.is_suit ? <button>#Suit</button> : null) : null}
-            {p_tags ? (p_tags.is_unique ? <button>#Unique</button> : null) : null}
-            {p_tags ? (p_tags.is_vehicle ? <button>#Vehicle</button> : null) : null}
-            {p_tags ? (p_tags.is_weapon ? <button>#Weapon</button> : null) : null}
+            {p_tags ? (p_tags.is_accessory ? <button className='tagButton'>#Accessory</button> : null) : null}
+            {p_tags ? (p_tags.is_substance ? <button className='tagButton'>#Substance</button> : null) : null}
+            {p_tags ? (p_tags.is_suit ? <button className='tagButton'>#Suit</button> : null) : null}
+            {p_tags ? (p_tags.is_unique ? <button className='tagButton'>#Unique</button> : null) : null}
+            {p_tags ? (p_tags.is_vehicle ? <button className='tagButton'>#Vehicle</button> : null) : null}
+            {p_tags ? (p_tags.is_weapon ? <button className='tagButton'>#Weapon</button> : null) : null}
             <Review newReview={newReview} reviews={reviews} setReviews={setReviews}/>
         </>
     )
