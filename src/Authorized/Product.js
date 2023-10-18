5f264653d6cd4376d715779e267fdd9bc6405458
Alonzo <alonzo@Alonzo>
2023-10-17 22:31:54 -0600

    Revamped single product view

--- src/Authorized/Product.js
+++ src/Authorized/Product.js
@@ -9,19 +9,23 @@ const Product = ({products, newReview, reviews, setReviews, productTags})=> {
     const p_tags = productTags.find((productTag) => productTag.product_id === product.id)
 
     return (
-        <>
-            <h1>{product.name}</h1>
-            <h3>Posted on {product.created_at.slice(0, 10)}</h3>
-            <p>{product.description}</p>
-            <img className='imageSize' src={product.image_url}></img><br/>
-            {p_tags ? (p_tags.is_accessory ? <button className='tagButton'>#Accessory</button> : null) : null}
-            {p_tags ? (p_tags.is_substance ? <button className='tagButton'>#Substance</button> : null) : null}
-            {p_tags ? (p_tags.is_suit ? <button className='tagButton'>#Suit</button> : null) : null}
-            {p_tags ? (p_tags.is_unique ? <button className='tagButton'>#Unique</button> : null) : null}
-            {p_tags ? (p_tags.is_vehicle ? <button className='tagButton'>#Vehicle</button> : null) : null}
-            {p_tags ? (p_tags.is_weapon ? <button className='tagButton'>#Weapon</button> : null) : null}
-            <Review newReview={newReview} reviews={reviews} setReviews={setReviews}/>
-        </>
+        <div className='singleProductContainer'>
+            <div className='singleProduct'>
+                <h1>{product.name}</h1>
+                <h2>Posted on {product.created_at.slice(0, 10)}</h2>
+                <p>{product.description}</p>
+                <img src={product.image_url}></img><br/>
+                {p_tags ? (p_tags.is_accessory ? <button className='tagButton'>#Accessory</button> : null) : null}
+                {p_tags ? (p_tags.is_substance ? <button className='tagButton'>#Substance</button> : null) : null}
+                {p_tags ? (p_tags.is_suit ? <button className='tagButton'>#Suit</button> : null) : null}
+                {p_tags ? (p_tags.is_unique ? <button className='tagButton'>#Unique</button> : null) : null}
+                {p_tags ? (p_tags.is_vehicle ? <button className='tagButton'>#Vehicle</button> : null) : null}
+                {p_tags ? (p_tags.is_weapon ? <button className='tagButton'>#Weapon</button> : null) : null}
+            </div>
+            <div className='reviews'>
+                <Review newReview={newReview} reviews={reviews} setReviews={setReviews}/>
+            </div>
+        </div>
     )
 };
 
--- src/Authorized/Reviews.js
+++ src/Authorized/Reviews.js
@@ -11,25 +11,29 @@ const Reviews = ({products, newReview, setReviews, reviews}) => {
         ev.preventDefault();
         const review = {title: title, description: description, stars: stars, productId: id}
         newReview({ id: id, setReviews: setReviews, review: review, reviews: reviews})
+        setTitle('')
+        setDescription('')
+        setStars('')
     };
-    const star = '<img src=\'../assets/img/favoriteNav.svg\'/>';
+    const star = '<img src=\'../assets/img/favorite.svg\'/>';
 
 return (
     <>
-    <h1>Add your Review!</h1>
-        <div>
-            <form onSubmit={ submit }>
-                <input placeholder='title' onChange={ev => setTitle(ev.target.value)} />
-                <input placeholder='description' onChange={ev => setDescription(ev.target.value)} />
-                <input placeholder='stars' onChange={ev => setStars(ev.target.value)} />
+    <h1 style={{textAlign: 'center'}}>Add your Review!</h1>
+        <div className={'formContainer'}>
+            <form className='editForm' onSubmit={ submit }>
+                <input placeholder='title' value={ title } onChange={ev => setTitle(ev.target.value)} />
+                <input placeholder='description' value={ description } onChange={ev => setDescription(ev.target.value)} />
+                <input placeholder='stars' value={ stars } onChange={ev => setStars(ev.target.value)} />
                 <button>Submit</button>
             </form>
+            <hr style={{border: '2px solid white', margin: '1em 0 1em 0'}}/>
             {
                 reviews.filter(review => review.productid === id).map(review => {
                     return(
                         <div key={review.id}>
                             <div id='rating'>
-                                <h1>{review.title}</h1> &nbsp;
+                                <h1>{review.title}</h1>
                                 {review.stars === 1 ? <div dangerouslySetInnerHTML={{__html: star}}></div> : null}
                                 {review.stars === 2 ? <div dangerouslySetInnerHTML={{__html: star + star}}></div> : null}
                                 {review.stars === 3 ? <div dangerouslySetInnerHTML={{__html: star + star + star}}></div> : null}
@@ -37,6 +41,7 @@ return (
                                 {review.stars === 5 ? <div dangerouslySetInnerHTML={{__html: star + star + star + star + star}}></div> : null}
                             </div>
                             <h3>{review.description}</h3>
+                            <hr style={{border: '1px solid white', margin: '1em 0 1em 0'}}/>
                         </div>
                     )
                 })
