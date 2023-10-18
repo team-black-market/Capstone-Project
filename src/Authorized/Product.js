7fe37d72b1fc2a54ca7bc77038c7b76a394deac0
Alonzo <alonzo@Alonzo>
2023-10-17 21:09:36 -0600

    Displaying tags on single product view, fixed minor edit bug.

--- src/Authorized/Product.js
+++ src/Authorized/Product.js
@@ -3,18 +3,24 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Review from './Reviews';
 
-const Product = ({products, newReview, reviews, setReviews})=> {
+const Product = ({products, newReview, reviews, setReviews, productTags={ productTags }})=> {
     const { id } = useParams();
     const product = products.find(product => product.id === id)
+    const p_tags = productTags.find((productTag) => productTag.product_id === product.id)
+    console.log(p_tags)
 
     return (
         <>
             <h1>{product.name}</h1>
-            <h3>Created at: {product.created_at}</h3>
+            <h3>Posted on {product.created_at.slice(0, 10)}</h3>
             <p>{product.description}</p>
             <img className='imageSize' src={product.image_url}></img><br/>
-            {/* <p>{setProductTags}</p> */}
-            <Link to="/products">Back</Link>
+            {p_tags.is_accessory ? <button>#Accessory</button> : null}
+            {p_tags.is_substance ? <button>#Substance</button> : null}
+            {p_tags.is_suit ? <button>#Suit</button> : null}
+            {p_tags.is_unique ? <button>#Unique</button> : null}
+            {p_tags.is_vehicle ? <button>#Vehicle</button> : null}
+            {p_tags.is_weapon ? <button>#Weapon</button> : null}
             <Review newReview={newReview} reviews={reviews} setReviews={setReviews}/>
         </>
     )
