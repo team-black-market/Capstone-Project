2064919505b41cc01db4ee0c084d8f9c3d7622cb
Alonzo <alonzo@Alonzo>
2023-10-17 21:15:20 -0600

    Fixed single product display bug

--- src/Authorized/Product.js
+++ src/Authorized/Product.js
@@ -3,11 +3,10 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Review from './Reviews';
 
-const Product = ({products, newReview, reviews, setReviews, productTags={ productTags }})=> {
+const Product = ({products, newReview, reviews, setReviews, productTags})=> {
     const { id } = useParams();
     const product = products.find(product => product.id === id)
     const p_tags = productTags.find((productTag) => productTag.product_id === product.id)
-    console.log(p_tags)
 
     return (
         <>
@@ -15,12 +14,12 @@ const Product = ({products, newReview, reviews, setReviews, productTags={ produc
             <h3>Posted on {product.created_at.slice(0, 10)}</h3>
             <p>{product.description}</p>
             <img className='imageSize' src={product.image_url}></img><br/>
-            {p_tags.is_accessory ? <button>#Accessory</button> : null}
-            {p_tags.is_substance ? <button>#Substance</button> : null}
-            {p_tags.is_suit ? <button>#Suit</button> : null}
-            {p_tags.is_unique ? <button>#Unique</button> : null}
-            {p_tags.is_vehicle ? <button>#Vehicle</button> : null}
-            {p_tags.is_weapon ? <button>#Weapon</button> : null}
+            {p_tags ? (p_tags.is_accessory ? <button>#Accessory</button> : null) : null}
+            {p_tags ? (p_tags.is_substance ? <button>#Substance</button> : null) : null}
+            {p_tags ? (p_tags.is_suit ? <button>#Suit</button> : null) : null}
+            {p_tags ? (p_tags.is_unique ? <button>#Unique</button> : null) : null}
+            {p_tags ? (p_tags.is_vehicle ? <button>#Vehicle</button> : null) : null}
+            {p_tags ? (p_tags.is_weapon ? <button>#Weapon</button> : null) : null}
             <Review newReview={newReview} reviews={reviews} setReviews={setReviews}/>
         </>
     )
